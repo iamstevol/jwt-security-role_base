ddd7960076ebb51362b7a4fe35a24e064f8d8cc2
iamstevol <stevol2015@gmail.com>
2023-05-22 00:12:18 +0100

    New commit

---
+++ src/main/java/com/iamstevol/jwtsecurityrole_base/JwtSecurityRoleBaseApplication.java
@@ -0,0 +1,13 @@
+package com.iamstevol.jwtsecurityrole_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityRoleBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecurityRoleBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iamstevol/jwtsecurityrole_base/JwtSecurityRoleBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamstevol.jwtsecurityrole_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityRoleBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
