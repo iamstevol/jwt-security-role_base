f8c36722d8103b89b3e7e2e22ecff84dd8af64a0
iamstevol <stevol2015@gmail.com>
2023-06-26 09:40:17 +0100

    Included the token secret and also the Jwt service

--- src/main/java/com/iamstevol/jwtsecurityrole_base/auth/AuthenticationService.java
+++ src/main/java/com/iamstevol/jwtsecurityrole_base/auth/AuthenticationService.java
@@ -1,4 +1,27 @@
 package com.iamstevol.jwtsecurityrole_base.auth;
 
+
+import com.iamstevol.jwtsecurityrole_base.user.User;
+import com.iamstevol.jwtsecurityrole_base.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    public AuthenticationResponse register(RegisterRequest request) {
+
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+
+        var savedUser = userRepository.save(user)
+    }
 }
